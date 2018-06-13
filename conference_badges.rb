2e2c75dc936ddd987f71688e3394040f55977585
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-13 06:34:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,11 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts 
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+end
+
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
