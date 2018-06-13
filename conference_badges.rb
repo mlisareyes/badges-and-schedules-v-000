26ac59b0f550d4ccf030c278062bc3710a0f2939
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-13 06:16:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |attendee|
+  attendees.map do |attendee|
     puts "Hello, my name is #{attendees}."
   end
 end
