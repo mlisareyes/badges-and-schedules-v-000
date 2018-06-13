62a532649e2799475d11617f78f5600218db2706
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-13 05:56:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,12 @@
 # Write your code here.
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  speakers.each do |name|
   puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |name|
+    puts "Hello, my name is #{name}."
+  end
 end
