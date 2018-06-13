9c897bc36a5cea1eb2f53b27862e2c7d41f3a7fd
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-13 06:27:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do |attendee, index|
-    puts "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  attendees.each_with_index.each do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
