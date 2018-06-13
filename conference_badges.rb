9c48b85d7def6973d2114404cd6390a846afa56a
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-13 05:48:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,8 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
+  speakers.each do |name|
   puts "Hello, my name is #{name}."
 end
+
+speakers.each do 
