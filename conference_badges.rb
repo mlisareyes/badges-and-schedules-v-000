65b33dabefade466a47220c7976fd3b6bc96686e
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-13 05:47:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_maker(name)
+def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
